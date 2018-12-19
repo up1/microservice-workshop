252b655d37281ad341db175eeb065413eff56a7f
up1 <somkiat.p@gmail.com>
2018-12-19 18:44:12 +0700

    Demo of API gateway

---
+++ 03-api-gateway/README.md
@@ -0,0 +1,16 @@
+# Example API gateway
+
+## Goal
+
+Create 3 services
+* Service 1
+* Service 2
+* API gateway
+
+## Steps
+
+1. `npm install` and `npm start`
+2. Call http://localhost:3001/
+3. Call http://localhost:3001/service1
+4. Call http://localhost:3001/service2
+5. Call `curl -H "Content-Type:application/xml" http://localhost:3001`
---
+++ 03-api-gateway/api_gateway.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const express = require('express')
+const httpProxy = require('express-http-proxy')
+const request = require('request-promise-native')
+const xml = require('xml')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+// Dummmy service discovery
+const service1Url = 'http://localhost:3002'
+const service2Url = 'http://localhost:3003'
+
+const service1Proxy = httpProxy(service1Url)
+const service2Proxy = httpProxy(service2Url)
+
+// Shared general logic: Authentication
+app.use((req, res, next) => {
+  // TODO: my authentication logic
+  console.log(`Authentication: ${req.path}`)
+  next()
+})
+
+// Aggregate services after authentication
+app.get('/', async (req, res) => {
+  const services = await Promise.all([
+    request({ uri: service1Url, json: true }),
+    request({ uri: service2Url, json: true })
+  ])
+
+  const response = { services }
+
+  // Format transformation: XML or JSON
+  if (req.get('Content-Type') === 'application/xml') {
+    const xmlResponse = xml(response)
+    res.set('content-type', 'text/xml')
+    res.end(xmlResponse)
+  } else {
+    res.json(response)
+  }
+})
+
+// Proxy request after authentication
+app.get('/service1', (req, res, next) => {
+  service1Proxy(req, res, next)
+})
+
+app.get('/service2', (req, res, next) => {
+  service2Proxy(req, res, next)
+})
+
+app.listen(port, () => {
+  console.info(`API Gateway is listening on port ${port}!`)
+})
