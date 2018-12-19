ec66c02fc7cb8ba66e84b9e7df5c969410353545
up1 <somkiat.p@gmail.com>
2018-12-20 00:23:40 +0700

    Working with circuit breaker

---
+++ 07-circuit-breaker/README.md
@@ -0,0 +1,11 @@
+# Example Circuit breaker
+
+## Goal
+
+Create circuit breaker of service
+
+## Steps
+
+1. `npm install` and `npm start`
+2. Call http://localhost:3002
+3. Call http://localhost:3002/?fail=true to open circuit breaker
---
+++ 07-circuit-breaker/service1.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const express = require('express')
+const CircuitBreaker = require('circuit-breaker-js')
+
+const app = express()
+const port = process.env.PORT || 3002
+
+// Circuit braker
+const breaker = new CircuitBreaker({
+  onCircuitOpen() {
+    console.log('CircuitBreaker is now open')
+  },
+  onCircuitClose() {
+    console.log('CircuitBreaker is now closed')
+  }
+})
+
+function valueableResource (isFail) {
+  return isFail ? Promise.reject(new Error('Resource error')) : Promise.resolve('Foo')
+}
+
+function protectedValueableResource (shouldFail) {
+  return new Promise((resolve, reject) => {
+    breaker.run((success, failed) => {
+      valueableResource(shouldFail)
+        .then((result) => {
+          success()
+          resolve(result)
+        })
+        .catch((err) => {
+          failed(err)
+          reject(err)
+        })
+    }, () => {
+      reject(new Error('Circuit breaker is active'))
+    })
+  })
+}
+
+app.get('/', (req, res, next) => {
+  // Force to fail => ?fail=true
+  const shouldFail = !!req.query.fail
+
+  protectedValueableResource(shouldFail)
+    .then((result) => {
+      res.send({ result })
+      next()
+    })
+    .catch((err) => {
+      res.send({ err: err.message })
+    })
+})
+
+app.listen(port, () => {
+  console.info(`Server listening on port ${port}!`)
+})
